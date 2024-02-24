3aeb8eaaeac043484e43d815a361c55fcd8c184b
Maksum Pinder <92977252+PinderMaksim@users.noreply.github.com>
2024-02-24 21:45:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+     //Task 1
+     int a=14;
+     int b=3;
+     double c= (double) a/b;
+        System.out.println(c);
+
+        //Task 2
+
+        int number = 65;
+        int g= number/10;
+        int k = number%10;
+        System.out.println(g);
+        System.out.println(k);
+
+
+    }
+}
